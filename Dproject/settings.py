3d6ac036e881bb4700f2e6d6e35133f8a097faee
Darkdev007 <tobilobaayodele23@gmail.com>
2022-01-20 19:47:23 -0100

    name

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i)zkvpl!bd1s1ktm$wa2vl314gp71yh!u#)x#b-g@^9q394$v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['3.87.69.18']
+ALLOWED_HOSTS = ['3.87.69.18']
 
 
 
