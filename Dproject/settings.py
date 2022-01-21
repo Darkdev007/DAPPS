e0a548632d064646a3886742ef1ae750cc5a90fc
Darkdev007 <tobilobaayodele23@gmail.com>
2022-01-21 16:59:06 -0100

    another settings change

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Senderhqq@gmail.com'
-EMAIL_HOST_PASSWORD = 'Maricopa20!'
+EMAIL_HOST_USER = 'tobilobaayodele23@gmail.com'
+EMAIL_HOST_PASSWORD = 'GODis2big6'
 
 
 # try:
