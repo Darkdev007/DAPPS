1c0ef5d4bb9e1301d1f530797b9808c9837929ee
Darkdev007 <tobilobaayodele23@gmail.com>
2022-01-21 14:12:42 -0100

    fix:image

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i)zkvpl!bd1s1ktm$wa2vl314gp71yh!u#)x#b-g@^9q394$v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
