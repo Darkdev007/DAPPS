e8d3757a5d889994077c9e15bf09657fd63fa384
Darkdev007 <tobilobaayodele23@gmail.com>
2022-01-21 10:10:53 -0100

    Removed email

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -138,14 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #SMTP CONFIGURATION
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Senderhqq@gmail.com'
-EMAIL_HOST_PASSWORD = 'Maricopa20!'
-# EMAIL_HOST_USER = 'Senderhqq@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Maricopa20!'
+
 
 try:
     from .local_settings import *
